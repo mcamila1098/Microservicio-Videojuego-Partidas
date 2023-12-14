ba8d97354a75bd6e6117841030cff239c1eb03fb
mcamila1098 <maria-alvarezp@unilibre.edu.co>
2023-12-14 18:40:56 -0500

    Documentación para MS webflux agregada

---
+++ src/main/java/com/example/videojuegopartidas/config/actuator/WebFluxConfig.java
@@ -0,0 +1,21 @@
+package com.example.videojuegopartidas.config.actuator;
+
+import com.example.videojuegopartidas.handler.ExampleHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Configuration
+public class WebFluxConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(ExampleHandler handler) {
+        return RouterFunctions
+                .route(GET("/example").and(accept(APPLICATION_JSON)), handler::getExample);
+    }
+}
